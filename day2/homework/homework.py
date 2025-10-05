aaccb2efe9c3ef14c6d1217bc6b6d2cf60f46b3e
bgharashvilimarii <mariambgharashvili@gmail.com>
2025-10-05 20:37:28 +0400

    second commit

---
+++ day2/homework/homework.py
@@ -0,0 +1,19 @@
+#დეკლარაცია არის ცვლადის შექმნა
+#ინიციალიზება არის ცვლადისთვის მნიშვნელობის მინიჭება
+#რეინიციალიზება არის ცვლადისთვის მნიშნელობის ხელახლა მინიჭება
+name="vashli"
+name="msxali"
+name="atami"
+print(name)
+
+num=5
+name=6
+item=9
+number=12
+pages=30
+print(num+name+item+number+pages)
+print(5+6+9+12+30)
+
+length=20
+width=10
+print(2*(20+10))
