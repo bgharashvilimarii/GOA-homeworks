71ad6a9491ae7bc7a6c9d6a0e3944fbefc9c4b52
bgharashvilimarii <mariambgharashvili@gmail.com>
2025-10-22 14:31:19 +0400

    second commit

---
+++ day6/homeworks/homework6.py
@@ -0,0 +1,16 @@
+#input  არის პროცესი როდესაც კომპიუტერში ხდება ინფორმაციის შესვლა მაგალითი-კლავიატურა
+#output არის პროცესი როდესაც პირიქით კომპიუტერიდან გამოდის ინფორმაცია-დინამიკი
+
+word=input("write any word you want: ")
+Word=input("write another word you want: ")
+
+print(word + Word)
+
+n=int(input("write first number: "))
+m=int(input("write second number: "))
+b=int(input("write third number: "))
+v=int(input("write fourth number: "))
+c=int(input("write fifth number: "))
+
+print((n+m+b+v+c)/5)
+
